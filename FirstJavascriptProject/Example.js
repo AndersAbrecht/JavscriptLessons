ba3b6a5765238b3ea20bb0ac6504aea6d384a3e6
Anders Brostrøm Abrecht <ande154s@kbhsyd.dk>
2020-01-14 14:59:58 +0100

    Yes

---
+++ FirstJavascriptProject/Example.js
@@ -0,0 +1,42 @@
+function textlengthcounter(textToMeasure) {  
+    return textToMeasure.length;    
+}
+
+function  {
+    textlengthcounter(textToMeasure)
+    return text
+}
+
+const txt = "7maG319";
+const txt2 = "nøfnøf"
+
+const sln = textlengthcounter(txt);
+const nøf = textlengthcounter(txt2); 
+const test3 = textlengthcounter("Hej test")
+
+const ison = false;
+const list = ["ost","is"]
+
+for (let x of list) {
+    console.log(x);
+}
+
+const is7 = txt.length <= 10 && (list.length == 2 || ison);
+console.log(is7);
+
+const president = "";
+
+if (president) {
+    console.log("fuckYEAH");
+
+    const f = 98;
+
+} else if (sln==7) {
+    console.log("That was different");
+
+} else {
+    console.log("Thissucks");
+}
+
+const f = 77;
+
---
+++ FirstJavascriptProject/index.js
@@ -0,0 +1,16 @@
+const names = [
+    "Kristian Videmark Parkov",
+    "Anders Abrecht",
+    "Anna Julie Paulsen"
+]
+
+for (let name of names){
+    const length = name.length;
+    const outputline = name + ": " + length;
+    console.log(outputline);
+}
+
+
+
+
+  
